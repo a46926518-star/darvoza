40fcbae669212acbb7f3b2297ef8d988a19f0452
a46926518-star <a46926518@gmail.com>
2026-04-26 09:46:39 +0500

    Redirect qo'shildi

--- darvoza/urls.py
+++ darvoza/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import redirect
+from django.views.generic import RedirectView
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -37,13 +37,15 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 
-    # Swagger yo'llari
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    path('', RedirectView.as_view(url='swagger/', permanent=False)),
 ]
 
 if settings.DEBUG:
