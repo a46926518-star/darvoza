7b833d5ea9df69a0c8e855035df900c95a8fa18c
a46926518-star <a46926518@gmail.com>
2026-04-26 11:25:36 +0500

    Path xatosi tuzatildi

--- darvoza/wsgi.py
+++ darvoza/wsgi.py
@@ -6,10 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(path, 'darvoza'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'darvoza.settings')
 
