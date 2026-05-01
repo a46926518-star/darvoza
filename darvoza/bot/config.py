85b179af5ee318d308ac0c6c2bf30c916e259ec6
a46926518-star <a46926518@gmail.com>
2026-05-01 18:47:50 +0500

    Fix: Render port binding issue and environment configuration

--- darvoza/bot/config.py
+++ darvoza/bot/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
+TOKEN = os.getenv("8684701971:AAEHDCEE2BoDYgI-VycfvUGUr6nYFtCzVp8")
+ADMIN_ID = os.getenv("8549599284")
 
 BASE_URL = os.getenv( "https://darvoza-1.onrender.com/api/")
 DJANGO_HOST = os.getenv("DJANGO_HOST", "http://127.0.0.1:8000")
